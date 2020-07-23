58a7946803efc48501f20bf221c773975b08f2d1
Amr Adel <amradelkhalil@gmail.com>
2020-07-23 14:46:05 +0200

    Modify the scaffolded Notes with the correct paths in both controller and the views, Make a smoke test for CRUDs operations

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,34 +1,26 @@
 class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
-  # GET /notes
-  # GET /notes.json
   def index
     @notes = Note.all
   end
 
-  # GET /notes/1
-  # GET /notes/1.json
   def show
   end
 
-  # GET /notes/new
   def new
     @note = Note.new
   end
 
-  # GET /notes/1/edit
   def edit
   end
 
-  # POST /notes
-  # POST /notes.json
   def create
-    @note = Note.new(note_params)
+    @note = Note.new(note_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.html { redirect_to user_note_url(current_user, @note), notice: 'Note was successfully created.' }
         format.json { render :show, status: :created, location: @note }
       else
         format.html { render :new }
@@ -37,12 +29,10 @@ class NotesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /notes/1
-  # PATCH/PUT /notes/1.json
   def update
     respond_to do |format|
       if @note.update(note_params)
-        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
+        format.html { redirect_to user_note_path(current_user, @note), notice: 'Note was successfully updated.' }
         format.json { render :show, status: :ok, location: @note }
       else
         format.html { render :edit }
@@ -51,23 +41,19 @@ class NotesController < ApplicationController
     end
   end
 
-  # DELETE /notes/1
-  # DELETE /notes/1.json
   def destroy
     @note.destroy
     respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
+      format.html { redirect_to user_notes_url(current_user), notice: 'Note was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_note
       @note = Note.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def note_params
       params.require(:note).permit(:title, :body)
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_115319) do
+ActiveRecord::Schema.define(version: 2020_07_23_121551) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
