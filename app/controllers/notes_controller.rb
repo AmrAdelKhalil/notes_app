10f798df89acf1d67183ca78b79de7ac5c36cfea
Amr Adel <amradelkhalil@gmail.com>
2020-07-23 14:08:26 +0200

    Add notes controller with CRUD methods, Add pry gem for development debugging

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,19 @@
+class NotesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
